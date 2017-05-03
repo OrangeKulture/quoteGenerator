e9f3833b5d3e2fb98cbb02a29196b6fe6952aaa4
Jorge A <ardonz@gmail.com>
2017-05-02 18:05:49 -0600

    minor changes

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 $(document).ready(function() {
-
+  $.ajaxSetup({ cache: false });
   setInterval(function() {
-    $('.mainH').css('visibility', 'visible').addClass('animated zoomIn');
-
+    $(".mainH").css("visibility", "visible").addClass("animated zoomIn");
   }, 755);
-
-  $('#mainB').on('click', function() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-
-        var resp = JSON.parse(xhttp.responseText);
-        var text = resp.quoteText;
-        var auth = resp.quoteAuthor;
-
-        var field1 = '<h3>' + '"' + text + '"' + '</h3>';
-        var field2 = '<h6>' + '- ' + auth + '</h6>';
-
-        document.getElementById('quote').innerHTML = field1;
-        document.getElementById('author').innerHTML = field2;
-
-        $('.content').css('visibility', 'visible');
+  function getQuote() {
+    var url =
+        "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+    $.ajax({
+      url: url,
+      type: "GET",
+      async: false,
+      dataType: "json",
+      success: function(data, status) {
+        var post = data.shift();
+        $("#quote").html(post.content);
+        $("#author p").text(post.title);
       }
-    };
+    });
+  }
 
-    xhttp.open("GET", "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", true);
-    xhttp.send();
-
+  $("#mainB").on("click", function() {
+    getQuote();
   });
 
+  getQuote();
 });
